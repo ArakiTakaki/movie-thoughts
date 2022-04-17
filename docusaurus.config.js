fa62a709853603ff47da90530c2139d9fe64b981
ArakiTakaki <arakitakaki.work@gmail.com>
2022-04-17 13:06:03 +0900

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,12 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/ArakiTakaki/movie-thoughts/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/ArakiTakaki/movie-thoughts/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
